5b6346a241525708a488415ce240cdb2daefd508
Nikola Jankovic <jankovic.gd@gmail.com>
2017-06-06 14:14:44 +0200

    new map section

---
+++ Map/NewSection/map6.py
@@ -0,0 +1,30 @@
+import folium
+import pandas
+
+data = pandas.read_csv("Volcanoes.txt")
+lat = list(data["LAT"])
+lon = list(data["LON"])
+elev = list(data["ELEV"])
+
+def color_producer(elevation):
+    if elevation < 1000:
+        return "green"
+    elif 1000 <= elevation < 3000:
+        return "orange"
+    else:
+        return "red"
+
+map = folium.Map(location = [38.58, -99.09], zoom_start = 6, tiles = "Mapbox Bright")
+
+fg = folium.FeatureGroup(name = "My Map")
+
+for lt, ln, ele in zip(lat, lon, elev):
+    fg.add_child(folium.CircleMarker(location=[lt, ln], radius = 6, color = "grey", fill_color = color_producer(ele), popup = str(ele) + " m", fill_opacity = 0.7))
+
+fg.add_child(folium.GeoJson(data=open('world.json', 'r', encoding='utf-8-sig'),
+                            style_function = lambda x: {'fillColor':'green' if x['properties']['POP2005'] < 10000000
+                                                                            else 'orange' if 10000000 <= x['properties']['POP2005'] < 20000000
+                                                                            else 'red'}))
+
+map.add_child(fg)
+map.save("Map6.html")
