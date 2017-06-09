0ebdeb673a01896d422ef8ee4ff3cdc49e5cbbb6
jankovicgd <jankovic.gd@gmail.com>
2017-06-09 14:48:01 +0200

    GUI learning

---
+++ GUI/assignment1.py
@@ -0,0 +1,30 @@
+from tkinter import *
+
+def kg_to_other():
+    g = float(e1_value.get()) * 1000
+    p = float(e1_value.get()) * 2.20462
+    o = float(e1_value.get()) * 35.274
+    t1.insert(END, g)
+    t2.insert(END, p)
+    t3.insert(END, o)
+
+window = Tk()
+
+l1 = Label(window, text="Kg")
+l1.grid(row = 0, column = 0)
+
+e1_value = StringVar()
+e1 = Entry(window, textvariable = e1_value)
+e1.grid(row = 0, column = 1)
+
+b1 = Button(window, text = "Convert", command = kg_to_other)
+b1.grid(row = 0, column = 2)
+
+t1 = Text(window, height = 1, width = 20)
+t1.grid(row = 1, column = 0)
+t2 = Text(window, height = 1, width = 20)
+t2.grid(row = 1, column = 1)
+t3 = Text(window, height = 1, width = 20)
+t3.grid(row = 1, column = 2)
+
+window.mainloop()
