9a4842df49f817ef973b6d2647cf93213243377d
jankovicgd <jankovic.gd@gmail.com>
2017-06-12 11:50:10 +0200

    scirpt sqlite

--- database/script1.py
+++ database/script1.py
@@ -14,8 +14,6 @@ def insert(item, quantity, price):
     conn.commit()
     conn.close
 
-# insert("Coffee Cup", 10, 5)
-
 def view():
     conn = sqlite3.connect("lite.db")
     cur = conn.cursor()
@@ -24,4 +22,21 @@ def view():
     conn.close()
     return rows
 
+def delete(item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("DELETE FROM store WHERE item = ?", (item,))
+    conn.commit()
+    conn.close()
+
+def update(quantity, price, item):
+    conn = sqlite3.connect("lite.db")
+    cur = conn.cursor()
+    cur.execute("UPDATE store SET quantity = ?, price=? WHERE item = ?", (quantity, price, item))
+    conn.commit()
+    conn.close()
+
+update(11, 6, "Water Glass")
+# delete('Wine Glass')
 print(view())
+# insert("Coffee Cup", 10, 5)
