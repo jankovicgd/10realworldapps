4ae70979e10f3d6d16176bebe345053a927493df
jankovicgd <jankovic.gd@gmail.com>
2017-07-19 13:29:02 +0200

    flask part 2

--- flask_db/app.py
+++ flask_db/app.py
@@ -1,5 +1,8 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, send_file
 from flask.ext.sqlalchemy import SQLAlchemy
+from send_email import send_email
+from sqlalchemy.sql import func
+from werkzeug import secure_filename
 
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:postgres123@localhost/height_collector'
@@ -22,16 +25,28 @@ def index():
 @app.route("/success", methods = ['POST'])
 def success():
     if request.method == 'POST':
+        #global file
         email = request.form["email_name"]
         height = request.form["height_name"]
-        print(email, height)
+        # file = request.files["files"] in case upload of file
+        # content = file.read()
+        # file.save(secure_filename("uploaded " + file.filename))
+        # with open("uploaded + file.filename") as f:
+        #   f.write("This was added later!")
+        # return render_template("index.html", btn = "download.html")
         if db.session.query(Data).filter(Data.email_ == email).count() == 0:
             data = Data(email, height)
             db.session.add(data)
             db.session.commit()
+            average_height = round(db.session.query(func.avg(Data.height_)).scalar(),1)
+            count = db.session.query(Data.height_).count()
+            send_email(email, height, average_height)
             return render_template("success.html")
     return render_template('index.html', text="Email in use")
 
+#@app.route("/download")
+#def download():
+#    return send_file("uploaded" + file.filename, attachment_filename="yourfile.csv", as_attachment=True)
 
 if __name__ == '__main__':
     app.debug = True
